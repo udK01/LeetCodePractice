a9f0940382014616b54724669346837d1b3d116d
udK01 <dominik.muranyi@gmail.com>
2024-10-20 23:38:04 +0100

    Completed Counter Challenge.

--- Counter.js
+++ Counter.js
@@ -33,12 +33,12 @@
  * @return {Function} counter
  */
 var createCounter = function (n) {
-  return function () {};
+  return function () {
+    return n++;
+  };
 };
 
-/**
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
+const counter = createCounter(10);
+console.log(counter()); // 10
+console.log(counter()); // 11
+console.log(counter()); // 12
