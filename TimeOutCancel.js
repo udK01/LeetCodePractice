a98e33205b8b1e223a46852968598d6d6f0ab254
udK01 <dominik.muranyi@gmail.com>
2024-10-29 22:58:01 +0000

    Completed TimeOutCancel Challenge.

--- TimeOutCancel.js
+++ TimeOutCancel.js
@@ -55,28 +55,38 @@
  * @param {number} t
  * @return {Function}
  */
-var cancellable = function (fn, args, t) {};
+var cancellable = function (fn, args, t) {
+  let x = setTimeout(() => {
+    fn(...args);
+  }, t);
 
-/**
- *  const result = [];
- *
- *  const fn = (x) => x * 5;
- *  const args = [2], t = 20, cancelTimeMs = 50;
- *
- *  const start = performance.now();
- *
- *  const log = (...argsArr) => {
- *      const diff = Math.floor(performance.now() - start);
- *      result.push({"time": diff, "returned": fn(...argsArr)});
- *  }
- *
- *  const cancel = cancellable(log, args, t);
- *
- *  const maxT = Math.max(t, cancelTimeMs);
- *
- *  setTimeout(cancel, cancelTimeMs);
- *
- *  setTimeout(() => {
- *      console.log(result); // [{"time":20,"returned":10}]
- *  }, maxT + 15)
- */
+  function cancelFn() {
+    clearTimeout(x);
+  }
+
+  return cancelFn;
+};
+
+const result = [];
+
+const fn = (x) => x * 5;
+const args = [2],
+  t = 20,
+  cancelTimeMs = 50;
+
+const start = performance.now();
+
+const log = (...argsArr) => {
+  const diff = Math.floor(performance.now() - start);
+  result.push({ time: diff, returned: fn(...argsArr) });
+};
+
+const cancel = cancellable(log, args, t);
+
+const maxT = Math.max(t, cancelTimeMs);
+
+setTimeout(cancel, cancelTimeMs);
+
+setTimeout(() => {
+  console.log(result); // [{"time":20,"returned":10}]
+}, maxT + 15);
