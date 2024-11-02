048ee9adf480ed75ea39343975ee4e172d26feaa
udK01 <dominik.muranyi@gmail.com>
2024-11-02 13:24:05 +0000

    Completed GroupBy Challenge.

--- GroupBy.js
+++ GroupBy.js
@@ -78,7 +78,19 @@
  * @param {Function} fn
  * @return {Object}
  */
-Array.prototype.groupBy = function (fn) {};
+Array.prototype.groupBy = function (fn) {
+  let result = {};
+  for (let i = 0; i < this.length; i++) {
+    let key = fn(this[i]);
+
+    if (!result[key]) {
+      result[key] = [];
+    }
+
+    result[key].push(this[i]);
+  }
+  return result;
+};
 
 /**
  * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
