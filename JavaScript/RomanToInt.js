4c4c79bdaa78da60f4dd0314888d956947cb7ed7
udK01 <dominik.muranyi@gmail.com>
2024-11-12 03:13:47 +0000

    Solved Roman Numeral To Int Challenge.

--- JavaScript/RomanToInt.js
+++ JavaScript/RomanToInt.js
@@ -48,4 +48,30 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function (s) {};
+var romanToInt = function (s) {
+  const values = new Map([
+    ["I", 1],
+    ["V", 5],
+    ["X", 10],
+    ["L", 50],
+    ["C", 100],
+    ["D", 500],
+    ["M", 1000],
+  ]);
+
+  let result = 0;
+
+  for (let i = 0; i < s.length - 1; i++) {
+    if (values.get(s[i]) < values.get(s[i + 1])) {
+      result -= values.get(s[i]);
+    } else {
+      result += values.get(s[i]);
+    }
+  }
+
+  return result + values.get(s[s.length - 1]);
+};
+
+console.log(romanToInt("III")); // 3
+console.log(romanToInt("LVIII")); // 58
+console.log(romanToInt("MCMXCIV")); // 1994
