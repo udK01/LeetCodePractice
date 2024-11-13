6bf85affe517e18d2842795a07d6d0d0eafc4d2a
udK01 <dominik.muranyi@gmail.com>
2024-11-13 22:48:53 +0000

    Completed Merge Sorted Lists.

--- JavaScript/MergeSortedLists.js
+++ JavaScript/MergeSortedLists.js
@@ -41,4 +41,22 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {};
+var mergeTwoLists = function (list1, list2) {
+  const dummy = new ListNode(-1);
+  let current = dummy;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  current.next = list1 || list2;
+
+  return dummy.next;
+};
