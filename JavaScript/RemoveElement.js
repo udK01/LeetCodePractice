30abda8a9512a607151ab48191f0002dc283ec14
udK01 <dominik.muranyi@gmail.com>
2024-11-14 00:59:38 +0000

    Completed Remove Element Challenge.

--- JavaScript/RemoveElement.js
+++ JavaScript/RemoveElement.js
@@ -52,4 +52,6 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function (nums, val) {};
+var removeElement = function (nums, val) {
+  nums.splice(0, nums.length, ...nums.filter((x) => x !== val));
+};
