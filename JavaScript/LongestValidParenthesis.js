2cede2377185f2f7e61b2225fa8a6396c47de3e2
udK01 <dominik.muranyi@gmail.com>
2024-11-15 05:06:34 +0000

    Solved Longest Valid Parenthesis Challenge.

--- JavaScript/LongestValidParenthesis.js
+++ JavaScript/LongestValidParenthesis.js
@@ -30,4 +30,24 @@
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function (s) {};
+var longestValidParentheses = function (s) {
+  if (s.length === 0) return 0;
+
+  let stack = [-1];
+  let longestCount = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(i);
+    } else {
+      stack.pop();
+      if (stack.length === 0) {
+        stack.push(i);
+      } else {
+        longestCount = Math.max(longestCount, i - stack[stack.length - 1]);
+      }
+    }
+  }
+
+  return longestCount;
+};
