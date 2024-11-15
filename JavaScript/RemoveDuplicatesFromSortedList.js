82f07b2922a7e38040ac1e59eb779c4ea4571f25
udK01 <dominik.muranyi@gmail.com>
2024-11-15 20:58:51 +0000

    Completed Remove Duplicates From Sorted List Challenge.

--- JavaScript/RemoveDuplicatesFromSortedList.js
+++ JavaScript/RemoveDuplicatesFromSortedList.js
@@ -35,4 +35,18 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function (head) {};
+var deleteDuplicates = function (head) {
+  if (head === null) return null;
+
+  let current = head;
+
+  while (current !== null && current.next !== null) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return head;
+};
