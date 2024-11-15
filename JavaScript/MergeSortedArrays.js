7c1a1df53e8ef30a3cef4a6c58e9cded405a9c4e
udK01 <dominik.muranyi@gmail.com>
2024-11-15 21:20:48 +0000

    Solved Merged Sorted Arrays Challenge.

--- JavaScript/MergeSortedArrays.js
+++ JavaScript/MergeSortedArrays.js
@@ -47,4 +47,11 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function (nums1, m, nums2, n) {};
+var merge = function (nums1, m, nums2, n) {
+  if (m === 0) return nums1.splice(0, n, ...nums2);
+  nums1.splice(
+    0,
+    nums1.length,
+    ...[...nums1.splice(0, m), ...nums2].sort((a, b) => a - b)
+  );
+};
