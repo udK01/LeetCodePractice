c965a66b01ac69b789578ea7eab859e91287d4db
udK01 <dominik.muranyi@gmail.com>
2024-11-15 08:34:43 +0000

    Completed Remove Shortest Sub Array Challenge.

--- JavaScript/RemoveShortestSubArray.js
+++ JavaScript/RemoveShortestSubArray.js
@@ -35,4 +35,33 @@
  * @param {number[]} arr
  * @return {number}
  */
-var findLengthOfShortestSubarray = function (arr) {};
+var findLengthOfShortestSubarray = function (arr) {
+  const n = arr.length;
+  let left = 0,
+    right = n - 1;
+
+  while (left < n - 1 && arr[left] <= arr[left + 1]) {
+    left++;
+  }
+
+  if (left === n - 1) return 0;
+
+  while (right > 0 && arr[right] >= arr[right - 1]) {
+    right--;
+  }
+
+  let minRemove = Math.min(n - left - 1, right);
+
+  let i = 0,
+    j = right;
+  while (i <= left && j < n) {
+    if (arr[i] <= arr[j]) {
+      minRemove = Math.min(minRemove, j - i - 1);
+      i++;
+    } else {
+      j++;
+    }
+  }
+
+  return minRemove;
+};
