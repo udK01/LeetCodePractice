cbb6e5234772c23ee307ed9c5413c96eb41b37d2
udK01 <dominik.muranyi@gmail.com>
2024-11-15 08:33:38 +0000

    Completed Add Binary Challenge.

--- JavaScript/AddBinary.js
+++ JavaScript/AddBinary.js
@@ -24,4 +24,6 @@
  * @param {string} b
  * @return {string}
  */
-var addBinary = function (a, b) {};
+var addBinary = function (a, b) {
+  return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);
+};
