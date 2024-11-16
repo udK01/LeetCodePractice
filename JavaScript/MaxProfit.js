9579667ba1ad9b0bde32ceeb74021394015bdf91
udK01 <dominik.muranyi@gmail.com>
2024-11-16 02:55:24 +0000

    Completed Max Profit Challenge.

--- JavaScript/MaxProfit.js
+++ JavaScript/MaxProfit.js
@@ -29,4 +29,17 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {};
+var maxProfit = function (prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+
+  for (let price of prices) {
+    if (price < minPrice) {
+      minPrice = price;
+    } else {
+      maxProfit = Math.max(maxProfit, price - minPrice);
+    }
+  }
+
+  return maxProfit;
+};
