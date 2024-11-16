e4f34dc1fd6a674bff4486b73d34ea2d2bb116d3
udK01 <dominik.muranyi@gmail.com>
2024-11-16 00:42:32 +0000

    Solved Is Same Tree Challenge.

--- JavaScript/IsSameTree.js
+++ JavaScript/IsSameTree.js
@@ -43,4 +43,11 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function (p, q) {};
+var isSameTree = function (p, q) {
+  if (!p && !q) return true;
+  if (!p || !q) return false;
+
+  if (p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
