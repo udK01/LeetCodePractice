4aeec2db56e54b7bd6ea71f597282c4a1c2448f9
udK01 <dominik.muranyi@gmail.com>
2024-11-18 04:34:42 +0000

    Missed Code From Previous Commit.

--- JavaScript/ExcelColumnTitle.js
+++ JavaScript/ExcelColumnTitle.js
@@ -36,4 +36,14 @@
  * @param {number} columnNumber
  * @return {string}
  */
-var convertToTitle = function (columnNumber) {};
+var convertToTitle = function (columnNumber) {
+  let columnTitle = "";
+
+  while (columnNumber > 0) {
+    columnNumber--;
+    columnTitle = String.fromCharCode((columnNumber % 26) + 65) + columnTitle;
+    columnNumber = Math.floor(columnNumber / 26);
+  }
+
+  return columnTitle;
+};
