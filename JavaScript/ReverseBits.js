d13194bab5f8dab3618dceacdd645b1ab6705da7
udK01 <dominik.muranyi@gmail.com>
2024-11-30 12:33:15 +0000

    Completed Reverse Bits Challenge.

--- JavaScript/ReverseBits.js
+++ JavaScript/ReverseBits.js
@@ -30,4 +30,11 @@
  * @param {number} n - a positive integer
  * @return {number} - a positive integer
  */
-var reverseBits = function (n) {};
+var reverseBits = function (n) {
+  let reversed = 0;
+  for (let i = 0; i < 32; i++) {
+    reversed = (reversed << 1) | (n & 1);
+    n >>= 1;
+  }
+  return reversed >>> 0;
+};
