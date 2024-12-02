cd8a0d6331eaa9537fb264c83df29a151e0a80da
udK01 <dominik.muranyi@gmail.com>
2024-12-02 17:52:47 +0000

    Completed Prefix Of A Word Challenge.

--- JavaScript/PrefixOfAWord.js
+++ JavaScript/PrefixOfAWord.js
@@ -37,4 +37,12 @@
  * @param {string} searchWord
  * @return {number}
  */
-var isPrefixOfWord = function (sentence, searchWord) {};
+var isPrefixOfWord = function (sentence, searchWord) {
+  const words = sentence.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].slice(0, searchWord.length) === searchWord) {
+      return i + 1;
+    }
+  }
+  return -1;
+};
