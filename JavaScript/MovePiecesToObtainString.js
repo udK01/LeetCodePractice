b9f25c7dad32f93ca031e1827d7f03705861a771
udK01 <dominik.muranyi@gmail.com>
2024-12-05 21:24:52 +0000

    Completed Move Pieces To Obtain String Challenge.

--- JavaScript/MovePiecesToObtainString.js
+++ JavaScript/MovePiecesToObtainString.js
@@ -41,4 +41,28 @@
  * @param {string} target
  * @return {boolean}
  */
-var canChange = function (start, target) {};
+var canChange = function (start, target) {
+  let n = start.length;
+
+  let i = 0,
+    j = 0;
+
+  while (i < n || j < n) {
+    while (i < n && start[i] === "_") i++;
+    while (j < n && target[j] === "_") j++;
+
+    if (i === n && j === n) return true;
+
+    if (i === n || j === n) return false;
+
+    if (start[i] !== target[j]) return false;
+
+    if (start[i] === "L" && i < j) return false;
+    if (start[i] === "R" && i > j) return false;
+
+    i++;
+    j++;
+  }
+
+  return true;
+};
