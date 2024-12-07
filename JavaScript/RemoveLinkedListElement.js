bc1ddfb1a82a9052e8724c3e3778c5a39d34e999
udK01 <dominik.muranyi@gmail.com>
2024-12-07 19:09:02 +0000

    Completed Remove Linked List Elements.

--- JavaScript/RemoveLinkedListElement.js
+++ JavaScript/RemoveLinkedListElement.js
@@ -37,4 +37,20 @@
  * @param {number} val
  * @return {ListNode}
  */
-var removeElements = function (head, val) {};
+var removeElements = function (head, val) {
+  while (head !== null && head.val === val) {
+    head = head.next;
+  }
+
+  let current = head;
+
+  while (current !== null && current.next !== null) {
+    if (current.next.val === val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return head;
+};
