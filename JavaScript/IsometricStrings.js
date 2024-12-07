eed833a43d1283ea07b54e3c5b6338667103d72e
udK01 <dominik.muranyi@gmail.com>
2024-12-07 19:19:02 +0000

    Completed Isometric String Challenge.

--- JavaScript/IsometricStrings.js
+++ JavaScript/IsometricStrings.js
@@ -47,4 +47,23 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function (s, t) {};
+var isIsomorphic = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  let mapS = {};
+  let mapT = {};
+
+  for (let i = 0; i < s.length; i++) {
+    let charS = s[i];
+    let charT = t[i];
+
+    if (mapS[charS] && mapS[charS] !== charT) return false;
+
+    if (mapT[charT] && mapT[charT] !== charS) return false;
+
+    mapS[charS] = charT;
+    mapT[charT] = charS;
+  }
+
+  return true;
+};
