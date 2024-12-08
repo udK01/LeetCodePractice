327ff67164655f0143f6998c93db87b258a7e59d
udK01 <dominik.muranyi@gmail.com>
2024-12-08 00:59:03 +0000

    Completed Contains Duplicates Challenge.

--- JavaScript/ContainsDuplicate.js
+++ JavaScript/ContainsDuplicate.js
@@ -39,4 +39,6 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function (nums) {};
+var containsDuplicate = function (nums) {
+  return new Set(nums).size !== nums.length;
+};
