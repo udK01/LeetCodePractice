7d05b39e2dcb8a42012683d7059376f0cf3fb25e
udK01 <dominik.muranyi@gmail.com>
2024-12-08 01:11:18 +0000

    Completed Contains Duplicates II Challenge.

--- JavaScript/ContainsDuplicatesII.js
+++ JavaScript/ContainsDuplicatesII.js
@@ -28,4 +28,19 @@
  * @param {number} k
  * @return {boolean}
  */
-var containsNearbyDuplicate = function (nums, k) {};
+var containsNearbyDuplicate = function (nums, k) {
+  const set = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return true;
+    }
+    set.add(nums[i]);
+
+    if (set.size > k) {
+      set.delete(nums[i - k]);
+    }
+  }
+
+  return false;
+};
