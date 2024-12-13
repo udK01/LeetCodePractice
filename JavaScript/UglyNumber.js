c2521bed09bda15c6fe6bbc49f8cb250edaccac1
udK01 <dominik.muranyi@gmail.com>
2024-12-13 18:26:02 +0000

    Solved Ugly Numbers Challenge.

--- JavaScript/UglyNumber.js
+++ JavaScript/UglyNumber.js
@@ -30,4 +30,16 @@
  * @param {number} n
  * @return {boolean}
  */
-var isUgly = function (n) {};
+var isUgly = function (n) {
+  if (n <= 0) return false;
+
+  const factors = [2, 3, 5];
+
+  for (const factor of factors) {
+    while (n % factor === 0) {
+      n /= factor;
+    }
+  }
+
+  return n === 1;
+};
