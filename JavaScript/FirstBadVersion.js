4ccc258f563027ca222631ff600529702c4e1c99
udK01 <dominik.muranyi@gmail.com>
2024-12-15 18:21:14 +0000

    Completed First Bad Version Challenge.

--- JavaScript/FirstBadVersion.js
+++ JavaScript/FirstBadVersion.js
@@ -45,5 +45,19 @@ var solution = function (isBadVersion) {
    * @param {integer} n Total versions
    * @return {integer} The first bad version
    */
-  return function (n) {};
+  return function (n) {
+    let left = 1,
+      right = n;
+
+    while (left < right) {
+      let mid = Math.floor(left + (right - left) / 2);
+      if (isBadVersion(mid)) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return left;
+  };
 };
