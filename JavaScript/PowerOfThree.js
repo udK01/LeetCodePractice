84014a8c10f9e1b2a3c7624049cb87fbf6f411f8
udK01 <dominik.muranyi@gmail.com>
2024-12-22 13:52:58 +0000

    Completed Power Of Three Challenge.

--- JavaScript/PowerOfThree.js
+++ JavaScript/PowerOfThree.js
@@ -32,4 +32,10 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfThree = function (n) {};
+var isPowerOfThree = function (n) {
+  if (n < 1) return false;
+  while (n % 3 === 0) {
+    n /= 3;
+  }
+  return n === 1;
+};
