48f9975c4b944501907475075f29f8bb3790a04e
udK01 <dominik.muranyi@gmail.com>
2024-12-23 20:17:53 +0000

    Completed Power Of Four Challenge.

--- JavaScript/PowerOfFour.js
+++ JavaScript/PowerOfFour.js
@@ -29,4 +29,10 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function (n) {};
+var isPowerOfFour = function (n) {
+  if (n < 1) return false;
+  while (n % 4 === 0) {
+    n /= 4;
+  }
+  return n === 1;
+};
