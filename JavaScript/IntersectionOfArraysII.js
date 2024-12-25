7fa0a0afeec7433198c8ae317814a48f8475e47a
udK01 <dominik.muranyi@gmail.com>
2024-12-25 08:34:27 +0000

    Completed Intersection Of Arrays II Challenge.

--- JavaScript/IntersectionOfArraysII.js
+++ JavaScript/IntersectionOfArraysII.js
@@ -30,4 +30,20 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersect = function (nums1, nums2) {};
+var intersect = function (nums1, nums2) {
+  const freqMap = {};
+  const result = [];
+
+  for (const num of nums1) {
+    freqMap[num] = (freqMap[num] || 0) + 1;
+  }
+
+  for (const num of nums2) {
+    if (freqMap[num] > 0) {
+      result.push(num);
+      freqMap[num]--;
+    }
+  }
+
+  return result;
+};
