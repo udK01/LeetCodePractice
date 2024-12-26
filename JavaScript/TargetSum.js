90dc11015e4052cb94d6881352bb9b6bf7b3a2f4
udK01 <dominik.muranyi@gmail.com>
2024-12-26 17:41:27 +0000

    Completed Target Sum Challenge.

--- JavaScript/TargetSum.js
+++ JavaScript/TargetSum.js
@@ -36,4 +36,21 @@
  * @param {number} target
  * @return {number}
  */
-var findTargetSumWays = function (nums, target) {};
+var findTargetSumWays = function (nums, target) {
+  const totalSum = nums.reduce((a, b) => a + b, 0);
+
+  if ((totalSum + target) % 2 !== 0 || totalSum < target) return 0;
+
+  const subsetSum = (totalSum + target) / 2;
+
+  const dp = new Array(subsetSum + 1).fill(0);
+  dp[0] = 1;
+
+  for (const num of nums) {
+    for (let sum = subsetSum; sum >= num; sum--) {
+      dp[sum] += dp[sum - num];
+    }
+  }
+
+  return dp[subsetSum];
+};
