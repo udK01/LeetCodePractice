a37b1645b1f8fd60a31727bd1b940cc450fef3ec
udK01 <dominik.muranyi@gmail.com>
2025-02-06 03:36:52 +0000

    Completed Tuples With Same Product Challenge.

--- JavaScript/TuplesWithSameProduct.js
+++ JavaScript/TuplesWithSameProduct.js
@@ -32,4 +32,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var tupleSameProduct = function (nums) {};
+var tupleSameProduct = function (nums) {
+  let count = 0;
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      let product = nums[i] * nums[j];
+      map.set(product, (map.get(product) || 0) + 1);
+    }
+  }
+
+  for (let freq of map.values()) {
+    if (freq > 1) {
+      count += 4 * (freq * (freq - 1));
+    }
+  }
+
+  return count;
+};
