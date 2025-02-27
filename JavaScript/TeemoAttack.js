5fecf624720359d485e4c4938ae933dde76a95f8
udK01 <dominik.muranyi@gmail.com>
2025-02-27 16:16:29 +0000

    Completed Teemo Attack Challenge.

--- JavaScript/TeemoAttack.js
+++ JavaScript/TeemoAttack.js
@@ -36,4 +36,16 @@
  * @param {number} duration
  * @return {number}
  */
-var findPoisonedDuration = function (timeSeries, duration) {};
+var findPoisonedDuration = function (timeSeries, duration) {
+  if (timeSeries.length === 0) return 0;
+
+  let totalDuration = 0;
+
+  for (let i = 0; i < timeSeries.length - 1; i++) {
+    totalDuration += Math.min(duration, timeSeries[i + 1] - timeSeries[i]);
+  }
+
+  totalDuration += duration;
+
+  return totalDuration;
+};
