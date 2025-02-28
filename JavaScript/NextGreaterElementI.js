0b2d196be5ca023f3dc81c6e572f3b9dcb638979
udK01 <dominik.muranyi@gmail.com>
2025-02-28 14:20:20 +0000

    Completed Next Greater Element Challenge.

--- JavaScript/NextGreaterElementI.js
+++ JavaScript/NextGreaterElementI.js
@@ -40,4 +40,17 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var nextGreaterElement = function (nums1, nums2) {};
+var nextGreaterElement = function (nums1, nums2) {
+  let res = [];
+  for (let i = 0; i < nums1.length; i++) {
+    let x = -1;
+    for (let j = nums2.indexOf(nums1[i]); j < nums2.length; j++) {
+      if (nums2[j] > nums1[i]) {
+        x = nums2[j];
+        break;
+      }
+    }
+    res.push(x);
+  }
+  return res;
+};
