810a2908f28c2ebbb8afc59ff68c20a1ac56febb
udK01 <dominik.muranyi@gmail.com>
2025-03-09 18:20:34 +0000

    Completed alternating groups challenge.

--- JavaScript/AlternatingGroups.js
+++ JavaScript/AlternatingGroups.js
@@ -24,4 +24,20 @@
  * @param {number} k
  * @return {number}
  */
-var numberOfAlternatingGroups = function (colors, k) {};
+var numberOfAlternatingGroups = function (colors, k) {
+  colors.push(...colors.slice(0, k - 1));
+  let count = 0;
+  let left = 0;
+
+  for (let right = 0; right < colors.length; right++) {
+    if (right > 0 && colors[right] === colors[right - 1]) {
+      left = right;
+    }
+
+    if (right - left + 1 >= k) {
+      count++;
+    }
+  }
+
+  return count;
+};
