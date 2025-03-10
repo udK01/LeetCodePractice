caff4b100dea4113bb6843b1fab26c90f903fb2b
udK01 <dominik.muranyi@gmail.com>
2025-03-10 19:09:37 +0000

    Completed detect capital challenge.

--- JavaScript/DetectCapital.js
+++ JavaScript/DetectCapital.js
@@ -27,4 +27,10 @@
  * @param {string} word
  * @return {boolean}
  */
-var detectCapitalUse = function (word) {};
+var detectCapitalUse = function (word) {
+  return (
+    word === word.toUpperCase() ||
+    word === word.toLowerCase() ||
+    word === word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+  );
+};
