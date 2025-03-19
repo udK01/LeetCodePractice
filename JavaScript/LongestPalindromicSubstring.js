e70ea7536e70dc00dae38c4455d795a1ca38b141
udK01 <dominik.muranyi@gmail.com>
2025-03-19 19:01:51 +0000

    Added Longest Palindrome Substring Question.

---
+++ JavaScript/LongestPalindromicSubstring.js
@@ -0,0 +1,53 @@
+// 5. Longest Palindromic Substring
+// Medium
+// Topics
+// Companies
+// Hint
+// Given a string s, return the longest palindromic substring in s.
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  if (s.length <= 1) return s;
+
+  let start,
+    maxLength = 0;
+
+  function expandAroundCenter(left, right) {
+    while (left >= 0 && right <= s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    return right - left - 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let len1 = expandAroundCenter(i, i);
+    let len2 = expandAroundCenter(i, i + 1);
+    let len = Math.max(len1, len2);
+
+    if (len > maxLength) {
+      maxLength = len;
+      start = i - Math.floor((len - 1) / 2);
+    }
+  }
+
+  return s.substring(start, start + maxLength);
+};
