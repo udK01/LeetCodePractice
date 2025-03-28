01d1605b899373f3ebeaa0699f2e8c2fbb794b24
udK01 <dominik.muranyi@gmail.com>
2025-03-28 17:09:37 +0000

    Added Longest Palindrome

---
+++ JavaScript/LongestPalindrome.js
@@ -0,0 +1,48 @@
+// 409. Longest Palindrome
+// Easy
+// Topics
+// Companies
+// Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+// Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+
+// Example 1:
+
+// Input: s = "abccccdd"
+// Output: 7
+// Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+// Example 2:
+
+// Input: s = "a"
+// Output: 1
+// Explanation: The longest palindrome that can be built is "a", whose length is 1.
+
+// Constraints:
+
+// 1 <= s.length <= 2000
+// s consists of lowercase and/or uppercase English letters only.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function (s) {
+  let charCount = new Map();
+  let length = 0;
+  let hasOdd = false;
+
+  for (let char of s) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+
+  for (let count of charCount.values()) {
+    if (count % 2 === 0) {
+      length += count;
+    } else {
+      length += count - 1;
+      hasOdd = true;
+    }
+  }
+
+  return hasOdd ? length + 1 : length;
+};
